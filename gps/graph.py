512cff942d5835679d5b984e4a71774707f39f0a
Joe <jozefnagyjr@gmail.com>
2014-10-18 00:08:44 -0400

    Added sample code to pull GPS records

--- gps/graph.py
+++ gps/graph.py
@@ -3,11 +3,43 @@
 
 import matplotlib.pyplot as plt
 import os
+from ConfigParser import *
+import datetime
+#import mysql.connector
+
+c = ConfigParser()
+c.read("../config.txt")
+
+db_name = c.get("DB", "cfg_db")
+db_user = c.get("DB", "cfg_db_user")
+db_passwd = c.get("DB", "cfg_db_passwd")
 
 #Create the directory to store the output files (graphs)
 if not os.path.exists("graphs"):
     os.makedirs("graphs")
 
+#Grab GPS data from database
+#cnx = mysql.connector.connect(user=db_user, database=db_name, password=db_passwd)
+#cursor = cnx.cursor()
+
+query = ("SELECT count(*) FROM gps "
+         "WHERE EventDate BETWEEN %s AND %s and uid=%s")
+
+dtStart = datetime.date(1999, 1, 1)
+dtEnd = datetime.date(1999, 12, 31)
+UID = 1
+
+cursor.execute(query, (dtStart,dtEnd,UID))
+
+#Add data into object to feed into plotter
+#for (first_name, last_name, hire_date) in cursor:
+#  print("{}, {} was hired on {:%d %b %Y}".format(
+#    last_name, first_name, hire_date))
+
+cursor.close()
+cnx.close()
+
+#Plot the GPS results to a image file
 plt.plot([1,2,3,4])
 plt.ylabel('some numbers')
 plt.savefig('graphs/test.png')
