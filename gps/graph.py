b2d3bad2ce1fc2b1628948786f72fd601afe2429
Joe <jozefnagyjr@gmail.com>
2014-10-17 23:20:19 -0400

    makes it's own directory for output of graphs

--- gps/graph.py
+++ gps/graph.py
@@ -2,6 +2,11 @@
 #Basic MatPlotLib example that exports hard coded figures to a text file on Linux server. 
 
 import matplotlib.pyplot as plt
+import os
+
+#Create the directory to store the output files (graphs)
+if not os.path.exists("graphs"):
+    os.makedirs("graphs")
 
 plt.plot([1,2,3,4])
 plt.ylabel('some numbers')
